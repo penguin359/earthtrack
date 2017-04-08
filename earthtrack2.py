b64245f6ac09c0c2b900502795710986e72ff256
Loren M. Lang <lorenl@north-winds.org>
2017-04-08 00:45:35 -0700

    Added example for Gnome Desktop

---
+++ earthtrack2.py
@@ -0,0 +1,1 @@
+earthtrack.py
---
+++ examples/gnome-desktop.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+./earthtrack2.py -x '-geometry 1280x800 -output /home/lorenl/Pictures/earth.png && gsettings set org.gnome.desktop.background picture-uri file:///home/lorenl/Pictures/earth.png'
